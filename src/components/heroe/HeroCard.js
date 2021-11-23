6a0c5fff39d691ca0a74024c540f3327fad46f46
JDemn <dsoftwaremancilla@outlook.es>
2021-11-23 00:54:21 -0600

    heroe card

---
+++ src/components/heroe/HeroCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const HeroCard = (
+    {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    }
+) => {
+    return (
+        <div className = "col">
+            <div className = "card">
+                {superhero} {id}
+            </div>
+        </div>
+    )
+}
